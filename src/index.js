9c90d8fe022a4acfd3dceed3706d72519b29a62c
Victor Tarapata <vvteach@yandex.ru>
2023-05-26 13:50:53 +0300

    feat: header; refact: adaptive fonts in root file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './pages/index.scss';
+
+const burgerMenuButton = document.querySelector('.menu__button');
+const burgerMenuIcon = burgerMenuButton.querySelector('.menu__button-icon')
+burgerMenuButton.addEventListener('click', () => {
+  burgerMenuIcon.classList.toggle('menu__button-icon_active');
+})
