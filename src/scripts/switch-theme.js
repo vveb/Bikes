b55c2a1c98a2d4c88f5e6e145aea16858b816f5a
Victor Tarapata <vvteach@yandex.ru>
2023-06-02 12:38:29 +0300

    feat: burger-menu for mobile complete

--- src/scripts/switch-theme.js
+++ src/scripts/switch-theme.js
@@ -1,21 +1,29 @@
 let theme = 0;
 const page = document.querySelector('.page');
-const switcherButton = document.querySelector('.theme-switcher__button');
-const switcherIconLight = document.querySelector('.theme-switcher__icon_type_light');
-const switcherIconDark = document.querySelector('.theme-switcher__icon_type_dark');
+const switcherButtons = document.querySelectorAll('.theme-switcher__button');
+const switcherIconsLight = document.querySelectorAll('.theme-switcher__icon_type_light');
+const switcherIconsDark = document.querySelectorAll('.theme-switcher__icon_type_dark');
 
 function switchTheme(newTheme) {
   if (theme === newTheme) {
     return;
   } else {
-    switcherButton.classList.toggle('theme-switcher__button_on');
+    switcherButtons.forEach((button) => {
+      button.classList.toggle('theme-switcher__button_on');
+    });
     page.classList.toggle('page_dark');
     theme = (theme + 1) % 2;
   }
 }
 
 export default function initThemeSwitcher() {
-  switcherButton.addEventListener('click', () => switchTheme(theme + 1));
-  switcherIconLight.addEventListener('click', () => switchTheme(0));
-  switcherIconDark.addEventListener('click', () => switchTheme(1))
+  switcherButtons.forEach((button) => {
+    button.addEventListener('click', () => switchTheme(theme + 1));
+  })
+  switcherIconsLight.forEach((icon) => {
+    icon.addEventListener('click', () => switchTheme(0));
+  })
+  switcherIconsDark.forEach((icon) => {
+    icon.addEventListener('click', () => switchTheme(1))
+  })
 }
